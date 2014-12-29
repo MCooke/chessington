518a7ddbde77e89944c686facdaf2fd0fa3c4676
Marcus Cooke <marcus.cooke@holidayextras.com>
2014-12-29 13:39:40 +0000

    Changed nav transitions.

--- _site/js/logic.js
+++ _site/js/logic.js
@@ -1,23 +1,12 @@
-console.log("Initlaise");
-
 $( document ).ready( function() {
-	// $('.do_bookOpen').on('click', function() {
-	// 	$( this ).toggleClass( 'active' );
-	// 	$( '.form-booking' ).toggleClass( 'active' );
-	// });
-	// $('.do_menu').on('click', function() {
-	// 	$( this ).toggleClass( 'active' );
-	// 	$( '.navbar-links' ).toggleClass( 'active' );
-	// });
-	// $('.do_datePicker').on('click', function() {
-	// 	console.log("Doin the datepicker");
-	// 	$( this ).toggleClass( 'active' );
-	// 	$( '.offcanvas_level' ).toggleClass( 'active' );
-	// });
 
-	$('.do_open').on('click', function(){
-		openTarget = $(this).attr('data-open');
+	$( '.do_open' ).on( 'click', function() {
+		openTarget = $( this ).attr( 'data-open' );
 		$( openTarget ).toggleClass('active');
-		console.log(openTarget);
+	} );
+
+	$( window ).on( 'click.small scroll.small' ,function(){
+		$( '.navbar_section-logo' ).addClass( 'small' );
+		$( this ).off( 'click.small scroll.small ');
 	});
 } );
